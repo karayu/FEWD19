ac663073a8e62be3abee096e00f278ab0a202912
Kara Yu <lele.yu@gmail.com>
2014-07-21 12:25:30 -0700

    Monday lab class

---
+++ Week_05_Intro_jQuery/12_lab/In_Class/starter_code/loops/js/exercise.js
@@ -0,0 +1,42 @@
+/**
+ * Javascript Exercise: Data Types
+ * Most of your answers should be stored in variables called q1, q2 etc.. and the variables printed to the console.
+ 
+ 	(i.e) console.log("Question 1" + q1)
+ */
+
+/*
+Hint: syntax for loops:
+array.forEach(function(element,index){
+  ...
+});
+
+for (var i = 0; i<array.length; i++) {
+	....
+}
+
+
+/**
+ * Question 1
+ * Create an array of numbers using 1,2,3, and 4 as values. 
+ * Use a loop (for loop or forEach) to increase each value by 1 and store the value back in the array.
+ * The end result should be an array of numbers with values 2,3,4,5
+ */
+
+
+/**
+* Question 2
+* Create an array of names with Will, Nevan, and Jessica as values. 
+* Append each of these names (with <li> and </li> around them) to the .class-list element using a loop. 
+* Hint: $(.class).append(); will append things to the class called .class
+*/
+
+/**
+ * Question 3
+ * Using the array from Question 1, find the average of the numbers in the array 
+ * Store the average in q3.
+ * Hint: (average=sum of all numbers/number of numbers). 
+ */
+
+
+
