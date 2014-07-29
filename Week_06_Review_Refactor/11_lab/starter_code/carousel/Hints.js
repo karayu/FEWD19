25c4a991da92f7e8ea152f8ca3d379931507a6cc
Kara Yu <lele.yu@gmail.com>
2014-07-28 17:24:47 -0700

    starter files

---
+++ Week_06_Review_Refactor/11_lab/starter_code/carousel/Hints.js
@@ -0,0 +1,25 @@
+var array = [1, 2, "hi", "bop"];
+
+
+//everything that runs should be in here
+$(document).ready(function () {
+	//...
+});
+
+
+//click listener for element with class called "class"
+$(".class").click(function() {
+	everything 
+});
+
+
+//what does this do?
+$("#elementId").attr("src","hi.jpg");
+ 
+
+ //what about this?
+ $("tag-name").val();
+
+
+//If you are confused by what any of the above things do, put it in console!  
+//Trying typing: $(".back") in console. 
